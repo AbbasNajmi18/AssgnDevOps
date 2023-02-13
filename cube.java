80fcf3ebf9e132b1b9a50d7d987c91e0d9f93985
AbbasNajmi18 <abbas.najmi@somaiya.edu>
2023-02-13 10:12:25 +0530

    cube.java

---
+++ cube.java
@@ -0,0 +1,9 @@
+import java.util;
+import java.util.Scanner;
+
+public class FindingCube {
+   public static void main(String args[]){
+      int num = 5;
+      System.out.println("Cube of the given number is "+(num*num*num));
+   }
+}
